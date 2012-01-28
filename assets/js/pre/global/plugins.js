40017972c481bc58141ed3cfe18a68269f9836d9
John Yoder <johngilesyoder@gmail.com>
2012-01-27 17:39:38 -0700

    Completed homepage static framework
    Added featured brands slider / added 'pre'compiled js folder / finished
    homepage for webkit browsers.

---
+++ assets/js/pre/global/plugins.js
@@ -0,0 +1,16 @@
+// JQUERY INNERFADE
+//@codekit-prepend "jquery.innerfade.js";
+
+// BOOTSTRAP PLUGINS
+//@codekit-append "../global/bootstrap/bootstrap-tooltip.js";
+//@codekit-append "../global/bootstrap/bootstrap-transition.js";
+//@codekit-append "../global/bootstrap/bootstrap-alert.js";
+//@codekit-append "../global/bootstrap/bootstrap-modal.js";
+//@codekit-append "../global/bootstrap/bootstrap-dropdown.js";
+//@codekit-append "../global/bootstrap/bootstrap-scrollspy.js";
+//@codekit-append "../global/bootstrap/bootstrap-tab.js";
+//@codekit-append "../global/bootstrap/bootstrap-popover.js";
+//@codekit-append "../global/bootstrap/bootstrap-button.js";
+//@codekit-append "../global/bootstrap/bootstrap-collapse.js";
+//@codekit-append "../global/bootstrap/bootstrap-carousel.js";
+//@codekit-append "../global/bootstrap/bootstrap-typeahead.js";
